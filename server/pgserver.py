bea9b38b18bb2cb9bea94e83443235ba4e7e17fb
Joshua Small <JMSmall89@gmail.com>
2023-05-14 13:50:56 -0400

    Update pgserver.py

--- server/pgserver.py
+++ server/pgserver.py
@@ -122,5 +122,7 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
-    # app.run()
+    # Get the port number assigned by Heroku (default is 5000 if running locally)
+    port = int(os.environ.get('PORT', 5000))
+    # Run the Flask application
+    app.run(host='0.0.0.0', port=port)
